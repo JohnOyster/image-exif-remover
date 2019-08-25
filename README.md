386de013d98c57609a4673624eb2b328f438e1be
John Oyster <25227550+JohnOyster@users.noreply.github.com>
2019-08-25 19:40:15 -0400

    Added base file using Pillow.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # image-exif-remover
-A python script to remove exif image data
+A python script to remove exif image data.
+
+This requires the Pillow library for Python3.
---
+++ remove-exif.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Simple EXIF Data Scrubber
+This is a simple Python script used to scrub exif data from photos before
+you send them out.
+
+"""
+import os
+import sys
+from PIL import Image
+
+# Define supported file types
+SUPPORTED_TYPES = ('.jpg')
+
+def scrub_image(filename):
+    print("[Info]\tScrubbing Image:   {}".format(filename))
+    image  = Image.open(filename)
+    data = list(image.getdata())
+    safe_image = Image.new(image.mode, image.size)
+    safe_image.putdata(data)
+    safe_filename = "safe_{}".format(filename) 
+    safe_image.save(safe_filename)
+    print("[INFO]\tExif data removed, saved as:   {}".format(safe_filename))
+
+def check_file(filename):
+    '''A helper function to make sure the argument passed in is a 
+    valid image file.
+    '''
+    is_file = os.path.isfile(filename)
+    is_image = filename.endswith(SUPPORTED_TYPES)
+    return is_file and is_image
+
+def help():
+    '''Displays the basic help information.'''
+    # TODO(John): Need to add help
+
+if __name__ == "__main__":
+    if sys.argv != 2:
+        help()
+        exit(1)
+    image_file = sys.argv[1]
+    if check_file(image_file)
+        scrub_image(sys.argv)
+    else:
+        print("[ERROR]\tInvalid File!")

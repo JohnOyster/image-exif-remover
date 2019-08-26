1992d454c39e5f1fc1f60cbfd4a5535ac7ce4756
John Oyster <25227550+JohnOyster@users.noreply.github.com>
2019-08-25 20:18:59 -0400

    Updated syntax errors and included test image.

--- remove-exif.py
+++ remove-exif.py
@@ -11,13 +11,14 @@ from PIL import Image
 # Define supported file types
 SUPPORTED_TYPES = ('.jpg')
 
-def scrub_image(filename):
-    print("[Info]\tScrubbing Image:   {}".format(filename))
-    image  = Image.open(filename)
+def scrub_image(image_file):
+    print("[Info]\tScrubbing Image:   {}".format(image_file))
+    image  = Image.open(image_file)
     data = list(image.getdata())
     safe_image = Image.new(image.mode, image.size)
     safe_image.putdata(data)
-    safe_filename = "safe_{}".format(filename) 
+    safe_filename = get_safe_name(image_file)
+    print(safe_filename) 
     safe_image.save(safe_filename)
     print("[INFO]\tExif data removed, saved as:   {}".format(safe_filename))
 
@@ -29,16 +30,23 @@ def check_file(filename):
     is_image = filename.endswith(SUPPORTED_TYPES)
     return is_file and is_image
 
+def get_safe_name(image_file):
+    '''Helper function to get file name appended with "safe_"'''
+    path, filename = os.path.split(image_file)
+    #filename = os.path.splitext(filename)[0]
+    new_filename = "safe_{}".format(filename)
+    return os.path.join(path, new_filename)
+
 def help():
     '''Displays the basic help information.'''
     # TODO(John): Need to add help
 
 if __name__ == "__main__":
-    if sys.argv != 2:
+    if len(sys.argv) != 2:
         help()
         exit(1)
     image_file = sys.argv[1]
     if check_file(image_file):
-        scrub_image(sys.argv)
+        scrub_image(image_file)
     else:
         print("[ERROR]\tInvalid File!")
